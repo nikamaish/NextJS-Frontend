9783f77c642b18474a69c50ecd60a98901dd6642
aish <aishnikam31@gmail.com>
2024-03-20 20:12:40 +0530

    user and bell icon

--- src/pages/components/Sidebar.js
+++ src/pages/components/Sidebar.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaUser, FaBell, FaMessage } from 'react-icons/fa';
+
 
 const Sidebar = () => {
   return (
     <aside className="bg-darkBlue w-64 h-screen">
-  {/* Sidebar Content Goes Here */}
- 
-</aside>
-
+      <div className="flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <FaUser className="text-white mr-2" />
+          <span className="text-white">Hello, User</span>
+        </div>
+        <FaBell className="text-white" />
+      </div>
+      <div>
+        
+      </div>
+      <hr />
+    </aside>
   );
 };
 
